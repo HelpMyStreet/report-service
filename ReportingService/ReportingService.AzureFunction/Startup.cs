8bc76ea0e29e92b63554eefb12dab968f22d00f5
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2021-09-30 09:53:37 +0100

    simplify

--- ReportingService/ReportingService.AzureFunction/Startup.cs
+++ ReportingService/ReportingService.AzureFunction/Startup.cs
@@ -3,21 +3,6 @@ using Microsoft.Azure.WebJobs.Host.Bindings;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Polly;
-using Polly.Extensions.Http;
-using ReportingService.CommunicationService;
-using ReportingService.Core.Configuration;
-using ReportingService.Core.Interfaces.Services;
-using ReportingService.Core.Utils;
-using ReportingService.RequestService;
-using ReportingService.Service;
-using ReportingService.UserService;
-using ReportingService.VerificationService;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 
 [assembly: FunctionsStartup(typeof(ReportingService.AzureFunction.Startup))]
 namespace ReportingService.AzureFunction
@@ -30,61 +15,6 @@ namespace ReportingService.AzureFunction
             ExecutionContextOptions executioncontextoptions = builder.Services.BuildServiceProvider()
                 .GetService<IOptions<ExecutionContextOptions>>().Value;
             string currentDirectory = executioncontextoptions.AppDirectory;
-
-            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
-                .SetBasePath(currentDirectory)
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
-
-            IConfigurationRoot config = configBuilder.Build();
-
-            Dictionary<HttpClientConfigName, ApiConfig> httpClientConfigs = config.GetSection("Apis").Get<Dictionary<HttpClientConfigName, ApiConfig>>();
-
-            var retryPolicy = HttpPolicyExtensions
-                .HandleTransientHttpError()
-                .RetryAsync(3);
-
-            IEnumerable<TimeSpan> timeSpans = new[]
-            {
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(5),
-                TimeSpan.FromSeconds(10),
-                TimeSpan.FromSeconds(20)
-            };
-
-            foreach (KeyValuePair<HttpClientConfigName, ApiConfig> httpClientConfig in httpClientConfigs)
-            {
-
-                builder.Services.AddHttpClient(httpClientConfig.Key.ToString(), c =>
-                {
-                    c.BaseAddress = new Uri(httpClientConfig.Value.BaseAddress);
-
-                    c.Timeout = httpClientConfig.Value.Timeout ?? new TimeSpan(0, 0, 0, 30);
-
-                    foreach (KeyValuePair<string, string> header in httpClientConfig.Value.Headers)
-                    {
-                        c.DefaultRequestHeaders.Add(header.Key, header.Value);
-                    }
-                    c.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
-                    c.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
-
-                }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-                {
-                    MaxConnectionsPerServer = httpClientConfig.Value.MaxConnectionsPerServer ?? 15,
-                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-                })
-                .AddTransientHttpErrorPolicy(x => x.WaitAndRetryAsync(timeSpans));
-            }
-
-            IConfigurationSection applicationConfigSettings = config.GetSection("ApplicationConfig");
-            builder.Services.Configure<ApplicationConfig>(applicationConfigSettings);
-
-            builder.Services.AddTransient<IHttpClientWrapper, HttpClientWrapper>();
-            builder.Services.AddSingleton<IReportsService, ReportsService>();
-            builder.Services.AddSingleton<IConnectCommunicationService, ConnectCommunicationService>();
-            builder.Services.AddSingleton<IConnectUserService, ConnectUserService>();
-            builder.Services.AddSingleton<IConnectRequestService, ConnectRequestService>();
-            builder.Services.AddSingleton<IConnectVerificationService, ConnectVerificationService>();
         }
     }
 }
